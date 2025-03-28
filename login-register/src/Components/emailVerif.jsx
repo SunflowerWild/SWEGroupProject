ebfd77b6e631565c46c11202336ea3a47695421f
Megan Mendez <megmend2185@gmail.com>
2025-03-28 01:09:04 -0400

    Made changes to email varification and home pages makeing sure there is a button to log out and a section to type in the code

--- login-register/src/Components/emailVerif.jsx
+++ login-register/src/Components/emailVerif.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Stack, TextField, Paper, Avatar, Checkbox, Grid, FormControlLabel, Button, Link, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blueGrey, indigo, } from '@mui/material/colors';
 
 export const EmailAuth = () =>{
+    const paperStyle = {padding :26,height: '20vh', width :480, margin:'20px auto'}
+    const avatarStyle = {backgroundColor: '#7a15bb9e'}
+    const btstyle={margin: '8px 0'}
     return(
-    <h1>Email Verification</h1>
+        <Grid container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: '100vh' }}>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align = 'center'>
+                <h1>Email Verification</h1>
+                <TextField id="outlined-basic" label="Verification Code" placeholder='Enter Code' variant="outlined" fullWidth required/>
+                </Grid>
+            </Paper>
+        </Grid>
     );
 }
--- login-register/src/Components/home.jsx
+++ login-register/src/Components/home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Stack, TextField, Paper, Avatar, Checkbox, Grid, FormControlLabel, Button, Link, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blueGrey, indigo, } from '@mui/material/colors';
 
+const theme = createTheme({
+    palette: {
+      primary: indigo,
+      secondary: blueGrey,
+    }
+});
 export const Home = () =>{
+    const paperStyle = {padding :26,height: '20vh', width :480, margin:'20px auto'}
+    const avatarStyle = {backgroundColor: '#7a15bb9e'}
+    const btstyle={margin: '8px 0'}
     return(
-        <h1>Welcome Home</h1>
+        <Grid container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: '100vh' }}>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align = 'center'>
+                <h1>Welcome home</h1>
+                
+                <ThemeProvider theme={theme}>
+                    <Button variant="contained" type='submit' color='primary' style={btstyle} fullWidth>Log Out</Button>
+                </ThemeProvider>
+                </Grid>
+            </Paper>
+        </Grid>
     );
-}
+};
+    
